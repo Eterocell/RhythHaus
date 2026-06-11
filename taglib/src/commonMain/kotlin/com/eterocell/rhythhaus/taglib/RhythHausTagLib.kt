b3642e93d51ac42313049ae0066b5f335fc837fe
Eterocell <eterocell@outlook.com>
2026-06-11 12:25:17 +0800

    Revert "feat: parse id3v1 metadata"
    This reverts commit 32f2536d2cd53597fa84da46efc4ef17f1501048.

--- taglib/src/commonMain/kotlin/com/eterocell/rhythhaus/taglib/RhythHausTagLib.kt
+++ taglib/src/commonMain/kotlin/com/eterocell/rhythhaus/taglib/RhythHausTagLib.kt
@@ -1,12 +1,10 @@
 package com.eterocell.rhythhaus.taglib
 
 import com.eterocell.rhythhaus.taglib.internal.TagTypeDetector
-import com.eterocell.rhythhaus.taglib.internal.id3.Id3v1Parser
 
 object RhythHausTagLib {
     fun read(bytes: ByteArray): TagReadResult = when (TagTypeDetector.detect(bytes)) {
-        TagFormat.ID3V1 -> Id3v1Parser.parse(bytes)
-        TagFormat.ID3V22, TagFormat.ID3V23, TagFormat.ID3V24 -> TagReadResult.Unsupported("Detected tag format is not implemented yet")
         null -> TagReadResult.Unsupported("No supported tag header found")
+        else -> TagReadResult.Unsupported("Detected tag format is not implemented yet")
     }
 }
