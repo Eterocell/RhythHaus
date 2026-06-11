7b0b9feaf6bc713912f091fde26891c250367b42
Eterocell <eterocell@outlook.com>
2026-06-11 12:25:17 +0800

    Revert "feat: parse id3v2 text metadata"
    This reverts commit 462dc1cf792d9cf03113eebc874018bd29f4dd1b.

--- taglib/src/commonMain/kotlin/com/eterocell/rhythhaus/taglib/RhythHausTagLib.kt
+++ taglib/src/commonMain/kotlin/com/eterocell/rhythhaus/taglib/RhythHausTagLib.kt
@@ -2,12 +2,11 @@ package com.eterocell.rhythhaus.taglib
 
 import com.eterocell.rhythhaus.taglib.internal.TagTypeDetector
 import com.eterocell.rhythhaus.taglib.internal.id3.Id3v1Parser
-import com.eterocell.rhythhaus.taglib.internal.id3.Id3v2Parser
 
 object RhythHausTagLib {
-    fun read(bytes: ByteArray): TagReadResult = when (val format = TagTypeDetector.detect(bytes)) {
+    fun read(bytes: ByteArray): TagReadResult = when (TagTypeDetector.detect(bytes)) {
         TagFormat.ID3V1 -> Id3v1Parser.parse(bytes)
-        TagFormat.ID3V22, TagFormat.ID3V23, TagFormat.ID3V24 -> Id3v2Parser.parse(bytes, format)
+        TagFormat.ID3V22, TagFormat.ID3V23, TagFormat.ID3V24 -> TagReadResult.Unsupported("Detected tag format is not implemented yet")
         null -> TagReadResult.Unsupported("No supported tag header found")
     }
 }
--- taglib/src/commonMain/kotlin/com/eterocell/rhythhaus/taglib/internal/id3/Id3v2Frame.kt
+++
@@ -1,6 +0,0 @@
-package com.eterocell.rhythhaus.taglib.internal.id3
-
-internal data class Id3v2Frame(
-    val id: String,
-    val payload: ByteArray,
-)
