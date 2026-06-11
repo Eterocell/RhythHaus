4d2fb5950d13dc9222ae04d96a85e3dfde658f4b
Eterocell <eterocell@outlook.com>
2026-06-11 12:25:17 +0800

    Revert "feat: detect supported tag formats"
    This reverts commit 294fa36be264c9ecf0d58591ddd6a46945127c81.

--- taglib/src/commonMain/kotlin/com/eterocell/rhythhaus/taglib/RhythHausTagLib.kt
+++ taglib/src/commonMain/kotlin/com/eterocell/rhythhaus/taglib/RhythHausTagLib.kt
@@ -1,10 +1,5 @@
 package com.eterocell.rhythhaus.taglib
 
-import com.eterocell.rhythhaus.taglib.internal.TagTypeDetector
-
 object RhythHausTagLib {
-    fun read(bytes: ByteArray): TagReadResult = when (TagTypeDetector.detect(bytes)) {
-        null -> TagReadResult.Unsupported("No supported tag header found")
-        else -> TagReadResult.Unsupported("Detected tag format is not implemented yet")
-    }
+    fun read(bytes: ByteArray): TagReadResult = TagReadResult.Unsupported("No supported tag header found")
 }
