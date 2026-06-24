76677615e066158fcc37579eac672992f5c38669
Eterocell <eterocell@outlook.com>
2026-06-24 12:07:02 +0800

    refactor: remove material3 dependency after miuix migration

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -95,7 +95,6 @@ kotlin {
             implementation(libs.miuix.ui)
             implementation(libs.compose.runtime)
             implementation(libs.compose.foundation)
-            implementation(libs.compose.material3)
             implementation(libs.compose.ui)
             implementation(libs.compose.components.resources)
             implementation(libs.compose.uiToolingPreview)
