00be6b3404c8a8cf572d4876fa260a02440305b7
Eterocell <eterocell@outlook.com>
2026-06-24 16:57:58 +0800

    fix: create RhythHaus Music folder at iOS app launch
    Previously the folder was only created when the user tapped Add Music
    Folder inside the app. This meant the folder didn't exist for Finder
    file sharing until after the user had already interacted with the app.
    Now the folder is created in the app init, before any UI loads.

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,22 @@
 import SwiftUI
+import Foundation
 
 @main
 struct iOSApp: App {
+    init() {
+        ensureMusicFolderExists()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+private func ensureMusicFolderExists() {
+    let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    guard let documentsUrl = urls.first else { return }
+    let musicFolder = documentsUrl.appendingPathComponent("RhythHaus Music")
+    try? FileManager.default.createDirectory(at: musicFolder, withIntermediateDirectories: true)
 }
