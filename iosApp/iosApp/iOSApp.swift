5f99adbca2553a70508fede947f86de3f816c0f6
Eterocell <eterocell@outlook.com>
2026-06-24 17:08:48 +0800

    fix: ensure Documents folder at iOS app launch

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,14 @@
 import SwiftUI
+import Foundation
 
 @main
 struct iOSApp: App {
+    init() {
+        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        guard let documentsUrl = urls.first else { return }
+        try? FileManager.default.createDirectory(at: documentsUrl, withIntermediateDirectories: true)
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
