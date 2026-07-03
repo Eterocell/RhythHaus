1bc6cc87ea43cf55c637587d1a98fa6f551c859f
Eterocell <eterocell@outlook.com>
2026-07-03 15:52:14 +0800

    refactor: remove unused verticalScroll from NowPlayingScreen

--- shared/src/commonMain/kotlin/com/eterocell/rhythhaus/NowPlayingScreen.kt
+++ shared/src/commonMain/kotlin/com/eterocell/rhythhaus/NowPlayingScreen.kt
@@ -3,10 +3,8 @@ package com.eterocell.rhythhaus
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
@@ -66,7 +64,6 @@ fun NowPlayingScreen(
             modifier = Modifier
                 .safeContentPadding()
                 .fillMaxSize()
-                .verticalScroll(rememberScrollState())
                 .padding(horizontal = 20.dp),
         ) {
             // Back button
